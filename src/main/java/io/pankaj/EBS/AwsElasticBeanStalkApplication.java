ed8bb83e540d6a70b7e25423ff2d0785504d5fac
Pankaj Garg <pankaj.garg@oodles.io>
2023-06-27 17:09:07 +0530

    Initial Commit

---
+++ src/main/java/io/pankaj/EBS/AwsElasticBeanStalkApplication.java
@@ -0,0 +1,13 @@
+package io.pankaj.EBS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticBeanStalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticBeanStalkApplication.class, args);
+	}
+
+}
